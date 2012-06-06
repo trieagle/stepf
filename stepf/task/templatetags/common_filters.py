7c76ba1f2bdcbec1ec64ea9d7611ac8f0bc08ae6
wfwei <cf.wfwei@gmail.com>
2012-06-06 15:42:21 +0800

    process bar demo

--- stepf/task/templatetags/common_filters.py
+++ stepf/task/templatetags/common_filters.py
@@ -7,3 +7,7 @@ def add_prefix(value, arg):
     """add some prefix for the given string"""
     return arg + str(value)
 
+@register.filter
+def get_range(value):
+    '''return a list [0,value-1]'''
+    return range(value)
