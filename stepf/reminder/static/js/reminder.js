4f94f6952e24fd81331ae5fda1bd896613d914f2
skyshaw <pxtian2008@gmail.com>
2012-07-17 12:45:57 +0800

    remove reminder_handlers

--- stepf/reminder/static/js/reminder.js
+++ stepf/reminder/static/js/reminder.js
@@ -1,37 +1,3 @@
-function delete_reminder_handler() {
-  var id_string = $(this).attr('id').replace('delete-reminder-', '');
-  var post_data = {'id': parseInt(id_string, 10)};
-  $.ajax({
-    url: '/reminder/remove_reminder/',
-    type: 'post',
-    dataType: 'json',
-    data: JSON.stringify(post_data),
-    success: function (removed) {
-      if (removed) {
-        $('#reminder-' + id_string).remove();
-      }
-    }
-  });
-  return false;
-}
-
-function done_reminder_handler() {
-  var id_string = $(this).attr('id').replace('done-reminder-', '');
-  var post_data = {'id': parseInt(id_string, 10)};
-  $.ajax({
-    url: '/reminder/done_reminder/',
-    type: 'post',
-    dataType: 'json',
-    data: JSON.stringify(post_data),
-    success: function (done) {
-      if (done) {
-        $('#reminder-' + id_string).remove();
-      }
-    }
-  });
-  return false;
-}
-
 function delete_reminder(reminder_id) {
   var post_delete_reminder = {'id': reminder_id};
   $.ajax({
